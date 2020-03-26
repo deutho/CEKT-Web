4f07c00c0c23f72d1ee7ac10b22988c7516c3446
Stefan Hinterhoelzl <hinterhoelzlstefan@gmail.com>
2020-03-26 13:01:16 +0100

    Erste HTML Page angelegt, user input mit javascript - alert auf den Bildschirm

---
+++ Webproject/script.js
@@ -0,0 +1,8 @@
+function getInput() {
+    var URL = document.getElementById('URL').value;
+    var ShortenedURL = document.getElementById('ShortURL').value;
+
+    alert(URL);
+    alert(ShortenedURL);
+
+}
