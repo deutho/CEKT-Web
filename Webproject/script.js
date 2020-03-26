15d48833d751d63ecd3d673bc84a60ee2f3439fd
Stefan Hinterhoelzl <hinterhoelzlstefan@gmail.com>
2020-03-26 17:44:33 +0100

    database an der richigen stellen, Javascript database Storing begonnen

--- Webproject/script.js
+++ Webproject/script.js
@@ -1,6 +1,23 @@
 function shorten() {
     var URL = document.getElementById('URL').value;
+    var shortenURL = "verkürzter Link"
 
-    document.getElementById("shorten").innerHTML = "Verkürzter Link: "+URL;
+    document.getElementById("shorten").innerHTML = "Verkürzter Link: " + shortenURL;
+    storeInDatabase(URL, shortenURL);
 
+}
+
+
+function storeInDatabase(URL, shortenURL) {
+    const sqlite3 = require('sqlite3').verbose();
+    let db = new sqlite3.Database(':memory:', (err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log("Verbingung zur in-memory SQLite Datenbank aufgebaut");
+    });
+
+    let sql = "INSERT INTO URL(URL, ShortenedURL) values("+URL+","+shortenURL+");";
+
+    
 }
