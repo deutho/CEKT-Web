aeb6a7d8972f5336ed9e90dd258f84fe622b6d72
Stefan Hinterhölzl <hinterhoelzlstefan@gmail.com>
2020-04-06 23:49:58 +0200

    Frontend - Post Delete Get

--- Frontend/shortly/src/app/app.component.ts
+++ Frontend/shortly/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
-import {ApiService} from './services/api.service';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './services/api.service';
 
-import {URL} from './classes/url';
+import { URL } from './classes/url';
 
 @Component({
   selector: 'app-root',
@@ -14,27 +14,58 @@ export class AppComponent {
   constructor(private _ApiService: ApiService) {
   }
 
-  url: Array <any> = [];
+  Allurl: Array<any> = [];
+  URLByID: URL = new URL();
+  URLPost: URL = new URL();
+  Message: String = "";
 
-  ngOnInit() {
-    this._ApiService.getAll()
+  ngOnInit() {}
+
+  public getAll() {
+    this._ApiService.getAllHTTP()
+      .subscribe
+      (
+        values => {
+          this.Allurl = values.data;
+        }
+
+      );
+  }
+
+  public getByID() {
+    this._ApiService.getByShortURLHTTP()
+      .subscribe
+      (
+        values => {
+          this.URLByID = values.data;
+        }
+      );
+  }
+
+  
+  
+  public PostURL() {
+    var toPost = new URL();
+    toPost.shortURL = (<HTMLInputElement>document.getElementById("PostShortURL")).value;
+    toPost.longURL = (<HTMLInputElement>document.getElementById("PostLongURL")).value;
+
+
+    this._ApiService.postURLHTTP(toPost)
     .subscribe
     (
       values =>
       {
-        this.url = values.data;
+        this.URLPost = values.data;
       }
-      
     );
 
-    this._ApiService.getByShortURL()
-    .subscribe
-    (
-      values =>
-      {
-        this.url = values.data
-      }
-    );
+  }
+
+  public DeleteURL() {
+
+    var shortURL = (<HTMLInputElement>document.getElementById("ShortURLDelete")).value;
+
+    this._ApiService.deleteURLHTTP(shortURL).subscribe();
   }
 
 }
--- Frontend/shortly/src/app/services/api.service.ts
+++ Frontend/shortly/src/app/services/api.service.ts
@@ -1,8 +1,10 @@
 import {Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import {HttpClient} from '@angular/common/http';
+import {URL } from '../classes/url';
+import { ThrowStmt } from '@angular/compiler';
 
-var apiurl = 'http://192.168.1.10:3000/';
+var apiurl = 'http://192.168.1.10:3000/url/';
 
 
 @Injectable()
@@ -11,13 +13,23 @@ export class ApiService
     
     constructor(private httpclient: HttpClient) {}
 
-    getAll(): Observable<any> {
-        return this.httpclient.get(apiurl+'URL');
+    getAllHTTP(): Observable<any> {
+        return this.httpclient.get(apiurl);
     }
 
 
-    getByShortURL(): Observable<any> {
-        let params1 = document.getElementById('sURL');
-        return this.httpclient.get(apiurl+params1);
+    getByShortURLHTTP(): Observable<any> {
+        var param = (<HTMLInputElement>document.getElementById("sURL")).value;
+        return this.httpclient.get(apiurl+param);
     }
+
+
+    postURLHTTP(toPost:URL): Observable<any> {
+        return this.httpclient.post(apiurl, toPost);
+    }
+
+    deleteURLHTTP(shortURL:string){
+        return this.httpclient.delete(apiurl+shortURL);
+    }
+
 }
