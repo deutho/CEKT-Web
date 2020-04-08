e1c35c61303050a25b674df34984bbbb96f560df
Stefan Hinterhölzl <hinterhoelzlstefan@gmail.com>
2020-04-08 22:52:18 +0200

    error handling

--- Frontend/shortly/src/app/redirect/redirect.component.ts
+++ Frontend/shortly/src/app/redirect/redirect.component.ts
@@ -10,31 +10,13 @@ import { ValueTransformer } from '@angular/compiler/src/util';
   styleUrls: ['./redirect.component.css']
 })
 export class RedirectComponent implements OnInit {
+  
 
   constructor(private _ApiService: ApiService, private router: Router) {
   }
   
-<<<<<<< HEAD
   ngOnInit(): void {
     this.redirectByID();
-=======
-longURL: string = ""; //url temp save for redireting url
-  
-
-
-  async ngOnInit(): Promise<void> {    
-
-    //get longurl for shorturl
-    await this.redirectByID();    
-
-    //sleep function
-    function sleep(ms) {
-      return new Promise(resolve => setTimeout(resolve, ms));
-    }
-
-    //redirect
-    sleep(2000).then(() => { this.redirect(this.longURL); });    
->>>>>>> 3dc3a34871c202c91015895dce42c34fdaaa07b6
   }
 
   //get longURL 
